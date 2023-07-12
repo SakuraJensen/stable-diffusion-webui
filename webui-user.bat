00ff1159c1b6fba18a2d13f8531c7570446f0bcd
Sakura Jensen <SapphiressKristi@gmail.com>
2023-07-11 21:19:06 -0400

    Nothing Useful
    Inital upload of my files.

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--listen --autolaunch --enable-insecure-extension-access --update-check --xformers
 
 call webui.bat
