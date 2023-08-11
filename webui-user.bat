29c0d3bac10d58123aa7d0f4e855b49e57aa84ca
Sakura Jensen <SapphiressKristi@gmail.com>
2023-08-11 12:42:16 -0400

    nothing usefulxl

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --autolaunch --enable-insecure-extension-access --update-check --xformers
+set COMMANDLINE_ARGS=--listen --enable-insecure-extension-access --no-half-vae --medvram --xformers
 
 call webui.bat
